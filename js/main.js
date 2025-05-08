253b70b1c31bb7b156ffe1ed8c21c4130bc61dbc
Chiara Cusi <cusichia@gmail.com>
2025-05-08 21:27:31 +0200

    checkbox

--- js/main.js
+++ js/main.js
@@ -23,9 +23,10 @@ const tasks = [
 
 // FUNCIONES
 function CompletedTask() {
+  listTasks.innerHTML = "";
     for (const task of tasks) {
         if (task.completed === true) {
-            listTasks.innerHTML += `<li class="completedTask"> <input type="checkbox" id="${task.id}"> ${task.name}</li>`;  
+            listTasks.innerHTML += `<li class="completedTask"> <input type="checkbox" checked id="${task.id}"> ${task.name}</li>`;  
       }
       else {
         listTasks.innerHTML += `<li> <input type="checkbox" id="${task.id}"> ${task.name} </li>`;
@@ -33,5 +34,34 @@ function CompletedTask() {
   }
 }
 
+/* function handleClickListTasks (event) {
+  let clickedTask = parseInt(event.target.id);
+  console.log(clickedTask);
+  const taskFound = tasks.find((task) => task.id === clickedTask);
+  console.log(taskFound);
+
+  CompletedTask()
+} */
+
+function handleClickListTasks (event) {
+  let clickedTask = parseInt(event.target.id);
+  console.log(clickedTask);
+  const taskFound = tasks.find((task) => task.id === clickedTask);
+  console.log(taskFound);
+
+  if (taskFound.completed === false) {
+    taskFound.completed = true;
+  } else {
+    taskFound.completed = false;
+  }
+  CompletedTask();
+}
+
+
+
 // LLAMAR FUNCIONES
-CompletedTask()
+CompletedTask();
+
+
+//EVENTOS
+listTasks.addEventListener("click", handleClickListTasks);
